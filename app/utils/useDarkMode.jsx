00e885b14cbcde62d7330cfbd7ac032f81849595
Atebi <156945353+Atebi@users.noreply.github.com>
2024-01-26 08:42:28 +0100

    Use darkmode

--- app/utils/useDarkMode.jsx
+++ app/utils/useDarkMode.jsx
@@ -4,19 +4,44 @@ import React, { useEffect, useState } from "react";
 
 const useDarkMode = () => {
   const [theme, setTheme] = useState(
-    Window !== "undefined" ? localStorage.theme : "dark",
+    typeof Window !== "undefined" && window.localStorage
+      ? localStorage.theme
+      : "dark",
   );
+
+  // typeof Window !== "undefined" && window.localStorage
+  //   ? localStorage.theme
+  //   : "dark",
+  // Window !== "undefined" ? localStorage.theme : "dark",
 
   const colorTheme = theme === "dark" ? "light" : "dark";
 
+  // const colorTheme = theme === "" ? "light" : theme;
+  // const colorTheme = theme;
+
   useEffect(() => {
+    // const mode =
+    //   typeof Window !== "undefined" && window.localStorage
+    //     ? localStorage.theme
+    //     : "dark";
+
+    // console.log("mode:", mode);
+
+    // setTheme(mode);
+    // console.log("mode theme", theme);
+
     const root = document.documentElement;
 
     root.classList.remove(colorTheme);
-    root.classList.add(theme);
+    // console.log("removed classlist from root:", colorTheme);
+
+    const skin = theme === "" ? "light" : theme;
+
+    root.classList.add(skin);
+    // console.log("added classlist to root:", skin);
 
     if (Window !== "undefined") {
-      localStorage.setItem("theme", theme);
+      localStorage.setItem("theme", skin);
     }
   }, [theme]);
 
